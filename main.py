1d455b0cafc8028c3019f9f23fcec50fd4e02bc1
Yaser <yaljamal@andrew.cmu.edu>
2024-11-15 23:00:11 +0300

    window class

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from cmu_graphics import *
 import time
 
+class Window:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+    def draw(self, app):
+        drawRect(app.width/2-self.w/2, app.height/2-self.h/2, self.w, self.h,
+        fill=app.ironGray, border='black', borderWidth=5)
+
 class Textbox:
     def __init__(self, x, y, w, h):
         self.x = x
@@ -11,7 +19,7 @@ class Textbox:
     def draw(self, app):
         drawRect(self.x, self.y, self.w, self.h, fill=None,
                     border=app.steelGray, borderWidth=5)
-        drawLabel(self.text[::-1][:25][::-1], self.x+20, self.y+self.h/2, align='left',
+        drawLabel(self.text[::-1][:25][::-1], self.x+100, self.y+self.h/2, align='left',
                     fill=app.steelGray, size=30)
 
 def onAppStart(app):
@@ -20,7 +28,9 @@ def onAppStart(app):
 
     app.steps = 0
 
-    mainTextBoxW = 400
+    app.window = Window(200, 200)
+
+    mainTextBoxW = 500
     mainTextBoxH = 80
     mainTextBox = Textbox(app.width/2-mainTextBoxW/2, \
         app.height/2-mainTextBoxH/2, mainTextBoxW, mainTextBoxH)
@@ -30,6 +40,7 @@ def redrawAll(app):
     drawRect(0, 0, app.width, app.height, fill=app.ironGray)
     for textbox in app.textboxes:
         textbox.draw(app)
+    app.window.draw(app)
 
 def onStep(app):
     app.steps += 1
@@ -41,13 +52,16 @@ def onStep(app):
 
 def onKeyHold(app, keys):
     if 'backspace' in keys:
-        app.textboxes[0].text = app.textboxes[0].text[:-1]
+        app.textboxes[0].text = app.textboxes[0].text[:-3] + \
+                                                    app.textboxes[0].text[-2:]
         time.sleep(0.1)
 
 def onKeyPress(app, key):
     if key == 'space':
-        app.textboxes[0].text = app.textboxes[0].text[:-2] + ' ' + app.textboxes[0].text[-2:]
+        app.textboxes[0].text = app.textboxes[0].text[:-2] + ' ' + \
+                                                    app.textboxes[0].text[-2:]
     elif key != 'backspace':
-        app.textboxes[0].text = app.textboxes[0].text[:-2] + key + app.textboxes[0].text[-2:]
+        app.textboxes[0].text = app.textboxes[0].text[:-2] + key + \
+                                                    app.textboxes[0].text[-2:]
 
 runApp(width=800, height=600)
