cbb220bfb4b8477ea501dae52ea90d686b7ae0ed
Yaser <yaljamal@andrew.cmu.edu>
2024-11-15 22:16:42 +0300

    first commit, yay

---
+++ main.py
@@ -0,0 +1,53 @@
+from cmu_graphics import *
+import time
+
+class Textbox:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.text = '| '
+    def draw(self, app):
+        drawRect(self.x, self.y, self.w, self.h, fill=None,
+                    border=app.steelGray, borderWidth=5)
+        drawLabel(self.text[::-1][:25][::-1], self.x+20, self.y+self.h/2, align='left',
+                    fill=app.steelGray, size=30)
+
+def onAppStart(app):
+    app.ironGray = rgb(109, 110, 113)
+    app.steelGray = rgb(224, 224, 224)
+
+    app.steps = 0
+
+    mainTextBoxW = 400
+    mainTextBoxH = 80
+    mainTextBox = Textbox(app.width/2-mainTextBoxW/2, \
+        app.height/2-mainTextBoxH/2, mainTextBoxW, mainTextBoxH)
+    app.textboxes = [mainTextBox]
+
+def redrawAll(app):
+    drawRect(0, 0, app.width, app.height, fill=app.ironGray)
+    for textbox in app.textboxes:
+        textbox.draw(app)
+
+def onStep(app):
+    app.steps += 1
+    if app.steps % 30 == 0:
+        if app.textboxes[0].text.endswith('| '):
+            app.textboxes[0].text = app.textboxes[0].text[:-2] + '  '
+        else:
+            app.textboxes[0].text = app.textboxes[0].text[:-2] + '| '
+
+def onKeyHold(app, keys):
+    if 'backspace' in keys:
+        app.textboxes[0].text = app.textboxes[0].text[:-1]
+        time.sleep(0.1)
+
+def onKeyPress(app, key):
+    if key == 'space':
+        app.textboxes[0].text = app.textboxes[0].text[:-2] + ' ' + app.textboxes[0].text[-2:]
+    elif key != 'backspace':
+        app.textboxes[0].text = app.textboxes[0].text[:-2] + key + app.textboxes[0].text[-2:]
+
+runApp(width=800, height=600)
