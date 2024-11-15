ab550e479e1cbe6adf8efcebe9c21d51e8851d48
Yaser <yaljamal@andrew.cmu.edu>
2024-11-16 00:13:14 +0300

    element superclass and neat class shortcuts and optimizations

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 from cmu_graphics import *
 import time
 
-class Window:
-    def __init__(self, w, h):
-        self.w = w
-        self.h = h
-    def draw(self, app):
-        drawRect(app.width/2-self.w/2, app.height/2-self.h/2, self.w, self.h,
-        fill=app.ironGray, border='black', borderWidth=5)
+class Element:
+    def checkMouseClick(self, mouseX, mouseY):
+        if self.x < mouseX < self.x + self.w and \
+            self.y < mouseY < self.y + self.h:
+            return True
+        return False
 
-class Textbox:
+class Textbox(Element):
     def __init__(self, x, y, w, h):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
-        self.text = '| '
+        self.text = '  '
+
     def draw(self, app):
         drawRect(self.x, self.y, self.w, self.h, fill=None,
-                    border=app.steelGray, borderWidth=5)
-        drawLabel(self.text[::-1][:25][::-1], self.x+100, self.y+self.h/2, align='left',
-                    fill=app.steelGray, size=30)
+                    border=app.steelGray, borderWidth=2)
+        drawLabel(self.text[::-1][:self.w//14][::-1], self.x+self.w/10,
+                    self.y+self.h/2, align='left', fill=app.steelGray, size=24)
+
+    def blinkCursor(self, clear=False):
+        if self.text.endswith('| ') or clear:
+            self.text = self.text[:-2] + '  '
+        else:
+            self.text = self.text[:-2] + '| '
+
+    def write(self, data):
+        self.text = self.text[:-2] + data + self.text[-2:]
+
+    def erase(self):
+        self.text = self.text[:-3] + self.text[-2:]
 
 def onAppStart(app):
     app.ironGray = rgb(109, 110, 113)
@@ -28,40 +40,40 @@ def onAppStart(app):
 
     app.steps = 0
 
-    app.window = Window(200, 200)
-
-    mainTextBoxW = 500
-    mainTextBoxH = 80
-    mainTextBox = Textbox(app.width/2-mainTextBoxW/2, \
-        app.height/2-mainTextBoxH/2, mainTextBoxW, mainTextBoxH)
-    app.textboxes = [mainTextBox]
+    titleBox = Textbox(app.width/2-500/2, \
+        200, 500, 50)
+    usernameBox = Textbox(app.width/2-500/2, \
+        280, 500, 50)
+    passwordBox = Textbox(app.width/2-500/2, \
+        360, 500, 50)
+    app.textboxes = [titleBox, usernameBox, passwordBox]
+    app.inFocusTB = 0
 
 def redrawAll(app):
     drawRect(0, 0, app.width, app.height, fill=app.ironGray)
     for textbox in app.textboxes:
         textbox.draw(app)
-    app.window.draw(app)
 
 def onStep(app):
     app.steps += 1
     if app.steps % 30 == 0:
-        if app.textboxes[0].text.endswith('| '):
-            app.textboxes[0].text = app.textboxes[0].text[:-2] + '  '
-        else:
-            app.textboxes[0].text = app.textboxes[0].text[:-2] + '| '
+        app.textboxes[app.inFocusTB].blinkCursor()
 
 def onKeyHold(app, keys):
     if 'backspace' in keys:
-        app.textboxes[0].text = app.textboxes[0].text[:-3] + \
-                                                    app.textboxes[0].text[-2:]
+        app.textboxes[app.inFocusTB].erase()
         time.sleep(0.1)
 
 def onKeyPress(app, key):
     if key == 'space':
-        app.textboxes[0].text = app.textboxes[0].text[:-2] + ' ' + \
-                                                    app.textboxes[0].text[-2:]
+        app.textboxes[app.inFocusTB].write(' ')
     elif key != 'backspace':
-        app.textboxes[0].text = app.textboxes[0].text[:-2] + key + \
-                                                    app.textboxes[0].text[-2:]
+        app.textboxes[app.inFocusTB].write(key)
+
+def onMousePress(app, mouseX, mouseY):
+    for i in range(len(app.textboxes)):
+        if app.textboxes[i].checkMouseClick(mouseX, mouseY):
+            app.textboxes[app.inFocusTB].blinkCursor(clear=True)
+            app.inFocusTB = i
 
 runApp(width=800, height=600)
